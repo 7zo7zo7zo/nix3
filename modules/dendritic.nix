5328cc8d6171443fb104d0a4bc958eed1f1a7179
7zo7zo7zo <77756772+7zo7zo7zo@users.noreply.github.com>
2026-06-02 14:43:28 -0700

    Fix import statement for flake-aspects module

--- modules/dendritic.nix
+++ modules/dendritic.nix
@@ -3,7 +3,6 @@
 {
 	imports = [
 		inputs.flake-parts.flakeModules.modules
-			inputs.flake-aspects.flakeModule
-			#inputs.home-manager.flakeModules.home-manager
+		inputs.flake-aspects.flakeModule
 	];
 }
