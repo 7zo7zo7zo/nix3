0ba27a83825449ef4c55c38d1095da3f812ad960
7zo7zo7zo <77756772+7zo7zo7zo@users.noreply.github.com>
2026-06-02 15:34:53 -0700

    Create xdg.nix

---
+++ modules/xdg.nix
@@ -0,0 +1,34 @@
+{
+	flake.aspects.xdg.homeManager = {
+		xdg = {
+			enable = true;
+
+			userDirs = {
+				enable = true;
+				setSessionVariables = true;
+				createDirectories = true;
+			};
+
+			mimeApps = {
+				enable = true;
+				defaultApplications = {
+					"application/pdf" = "org.pwmt.zathura.desktop";
+
+					"image/bmp" = "nsxiv.desktop";
+					"image/gif" = "nsxiv.desktop";
+					"image/jpg" = "nsxiv.desktop";
+					"image/png" = "nsxiv.desktop";
+					"image/x-tga" = "nsxiv.desktop";
+					"image/webp" = "nsxiv.desktop";
+					"image/heic" = "nsxiv.desktop";
+					"image/svg+xml" = "nsxiv.desktop";
+
+					"video/mkv" = "mpv.desktop";
+					"video/quicktime" = "mpv.desktop";
+					"video/mp4" = "mpv.desktop";
+					"video/webm" = "mpv.desktop";
+				};
+			};
+		};
+	};
+}
