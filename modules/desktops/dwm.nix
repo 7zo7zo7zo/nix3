d389df190edfb457e3e45a139bd9900ca1f77bbf
7zo7zo7zo <7zo7zo7zo@gmail.com>
2026-06-24 01:30:25 -0700

    use hm xsession so picom loads

--- modules/desktops/dwm.nix
+++ modules/desktops/dwm.nix
@@ -7,6 +7,9 @@
 
 			nixos = { pkgs, ... }: {
 				services = {
+					displayManager = {
+						defaultSession = "none+dwm"; # doesn't do anything
+					};
 					xserver = {
 						enable = true;
 						autoRepeatDelay = 200;
@@ -44,6 +47,25 @@
 			homeManager = { config, ... }: {
 				my.scripts = [ "screenshot" ];
 
+				xsession = {
+					enable = true;
+					scriptPath = "${config.xdg.cacheHome}/X11/xsession"; # technically this should be a local path from $HOME
+					windowManager.command = "exec dwm";
+					profileExtra = ''
+						# https://nixos.wiki/wiki/Using_X_without_a_Display_Manager
+						if test -z "$DBUS_SESSION_BUS_ADDRESS"; then
+							eval $(dbus-launch --exit-with-session --sh-syntax)
+						fi
+						systemctl --user import-environment DISPLAY XAUTHORITY
+						
+						if command -v dbus-update-activation-environment >/dev/null 2>&1; then
+							dbus-update-activation-environment DISPLAY XAUTHORITY
+						fi
+
+						dwmblocks &
+					'';
+				};
+
 				home.sessionVariables = {
 					XINITRC = "${config.xdg.configHome}/X11/xinitrc";
 				};
@@ -51,18 +73,8 @@
 				xdg.configFile."X11/xinitrc" = {
 					executable = true;
 					text = ''
-						# https://nixos.wiki/wiki/Using_X_without_a_Display_Manager
-						if test -z "$DBUS_SESSION_BUS_ADDRESS"; then
-							eval $(dbus-launch --exit-with-session --sh-syntax)
-						fi
-							systemctl --user import-environment DISPLAY XAUTHORITY
-
-						if command -v dbus-update-activation-environment >/dev/null 2>&1; then
-							dbus-update-activation-environment DISPLAY XAUTHORITY
-						fi
-
-						dwmblocks &
-						exec dwm
+						export USERXSESSION="${config.xdg.cacheHome}/X11/xsession"
+						$USERXSESSION
 					'';
 				};
 
